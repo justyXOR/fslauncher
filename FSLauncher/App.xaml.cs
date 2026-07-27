0f9ee1e2184ffe2b4f3d3092597d94742f0219b2
Ramir Baykin <f_lambda_x@proton.me>
2026-07-27 14:59:15 +0300

    Initial commit

---
+++ FSLauncher/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace FSLauncher;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
